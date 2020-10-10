f48232b9ce9da28531ed9f6168439faaa79c96dd
Mukesh Murugan <iammukeshm@gmail.com>
2020-10-10 17:25:56 +0530

    added cmplete account services to webapi

--- AspNetCoreHero.PublicAPI/Controllers/AccountController.cs
+++ AspNetCoreHero.PublicAPI/Controllers/AccountController.cs
@@ -25,6 +25,30 @@ namespace AspNetCoreHero.PublicAPI.Controllers
         {
             return Ok(await _accountService.AuthenticateAsync(request, GenerateIPAddress()));
         }
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync(RegisterRequest request)
+        {
+            var origin = Request.Headers["origin"];
+            return Ok(await _accountService.RegisterAsync(request, origin));
+        }
+        [HttpGet("confirm-email")]
+        public async Task<IActionResult> ConfirmEmailAsync([FromQuery] string userId, [FromQuery] string code)
+        {
+            var origin = Request.Headers["origin"];
+            return Ok(await _accountService.ConfirmEmailAsync(userId, code));
+        }
+        [HttpPost("forgot-password")]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordRequest model)
+        {
+            await _accountService.ForgotPassword(model, Request.Headers["origin"]);
+            return Ok();
+        }
+        [HttpPost("reset-password")]
+        public async Task<IActionResult> ResetPassword(ResetPasswordRequest model)
+        {
+
+            return Ok(await _accountService.ResetPassword(model));
+        }
         private string GenerateIPAddress()
         {
             if (Request.Headers.ContainsKey("X-Forwarded-For"))
